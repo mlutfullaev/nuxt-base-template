3fdeed5bdca93c868b4cbead9eb37521362ec52f
magalutfullaev <mhoja9494@gmail.com>
2024-09-24 10:15:28 +0500

    add: Tailwind & Shadcn & scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  css: ['@/assets/styles/index.scss'],
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./components/ui",
+  },
 })
